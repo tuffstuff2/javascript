de964df66de1ed347304aefb3ecdd0183b610dac
tuffstuff2 <131016825+tuffstuff2@users.noreply.github.com>
2024-09-19 18:13:55 +0500

    Add files via upload

--- java-script/js/index.js
+++ java-script/js/index.js
@@ -2771,61 +2771,451 @@
 // console.log(min);
 // console.log((ever /= users.length));
 
-const cars = [
+// const cars = [
+//   {
+//     model: "malibu",
+//     mark: "gm",
+//     price: 22000,
+//     year: 2018,
+//     mileage: 64000,
+//     horsePower: 212,
+//   },
+//   {
+//     model: "cobalt",
+//     mark: "gm",
+//     price: 11000,
+//     year: 2023,
+//     mileage: 4000,
+//     horsePower: 190,
+//   },
+//   {
+//     model: "cls-63",
+//     mark: "mercedes-benz",
+//     price: 64000,
+//     year: 2010,
+//     mileage: 120000,
+//     horsePower: 450,
+//   },
+//   {
+//     model: "competition",
+//     mark: "BMW",
+//     price: 120000,
+//     year: 2020,
+//     mileage: 20000,
+//     horsePower: 520,
+//   },
+//   {
+//     model: "k5",
+//     mark: "kia",
+//     price: 32000,
+//     year: 2024,
+//     mileage: 1000,
+//     horsePower: 240,
+//   },
+// ];
+// let faster = cars[0];
+// let mil = cars[0];
+// let young = cars[0];
+// cars.forEach((care) => {
+//   if (care.horsePower > faster.horsePower) {
+//     faster = care;
+//   }
+//   if (care.mileage > mil.mileage) {
+//     mil = care;
+//   }
+//   if (care.year > young.year) {
+//     young = care;
+//   }
+// });
+
+// console.log(faster, mil, young);
+
+// let tree = +prompt("Введите число:");
+// let stars = '';
+// let spaces = '';
+
+// for (let i = 0; i < tree; i++) {
+//   spaces = '';
+//   for (let j = 0; j < tree - i - 1; j++) {
+//     spaces += ' ';
+//   }
+//   stars += '*';
+//   console.log(spaces + stars + stars.slice(1));
+// }
+
+// let fibonachi = +prompt('Введите число')
+// let a = 0
+// let b = 1
+// let c;
+// for (let i = 0; i < fibonachi; i++) {
+//   c = a + b
+//   a = b
+//   b = c
+// }
+// console.log(b);
+
+// // задание показать только те машины тем кому не больше двух лет
+// let cars = [
+//   {
+//     model: "cobalt",
+//     company: "general motors",
+//     engine: 1.5,
+//     price: 15000,
+//     beatan: true,
+//     year: 2022,
+//     mileage: 13000,
+//     tech_passport: {
+//       date: 2022,
+//       exp_date: 2032,
+//     },
+//   },
+//   {
+//     model: "malibu",
+//     company: "general motors",
+//     color: "dry asphalt",
+//     engine: 2.4,
+//     price: 30000,
+//     beaten: false,
+//     year: 2022,
+//     mileage: 1000,
+//     tech_passport: {
+//       date: 2022,
+//       exp_date: 2032,
+//     },
+//   },
+//   {
+//     model: "tracker",
+//     company: "general motors",
+//     color: "black",
+//     engine: "2.2",
+//     price: 22000,
+//     beaten: false,
+//     year: 2021,
+//     mileage: 2000,
+//     tech_passport: {
+//       date: 2021,
+//       exp_date: 3031,
+//     },
+//   },
+//   {
+//     model: "gentra",
+//     company: "general motors",
+//     color: "black",
+//     engine: 1.5,
+//     price: 17000,
+//     beaten: false,
+//     year: 2021,
+//     mileage: 6000,
+//     tech_passport: {
+//       date: 2021,
+//       exp_date: 2031,
+//     },
+//   },
+//   {
+//     model: "Damas",
+//     company: "Chevrolet",
+//     color: "White",
+//     engine: 2.4,
+//     price: 10000,
+//     beaten: false,
+//     year: 2023,
+//     mileage: 100,
+//     tech_pasport: {
+//       date: 2023,
+//       exp_date: 2033,
+//     },
+//   },
+//   {
+//     model: "Spark",
+//     company: "general motors",
+//     color: "dirtygreen",
+//     engine: 1.25,
+//     price: 7000,
+//     beaten: true,
+//     year: 2008,
+//     mileage: 328000,
+//     tech_passport: {
+//       date: 2008,
+//       exp_date: 2023,
+//     },
+//   },
+//   {
+//     model: "Labo",
+//     company: "GM ",
+//     color: "white",
+//     engine: 0.8,
+//     price: 10000,
+//     beaten: false,
+//     year: 2023,
+//     mileage: 0,
+//     tech_passport: {
+//       date: 2023,
+//       exp_date: 2033,
+//     },
+//   },
+//   {
+//     model: "tico",
+//     company: "daewoo",
+//     color: "black",
+//     engine: 1.5,
+//     price: 10000,
+//     beaten: true,
+//     year: 2010,
+//     mileage: 1000,
+//     tech_passport: {
+//       date: 2022,
+//       exp_date: 2025,
+//     },
+//   },
+//   {
+//     model: "traverse",
+//     company: "chevrolet",
+//     color: "black",
+//     engine: 3.6,
+//     price: 60000,
+//     beaten: false,
+//     year: 2023,
+//     mileage: 500,
+//     tech_passport: {
+//       date: 2023,
+//       exp_date: 2033,
+//     },
+//   },
+//   {
+//     model: "volga",
+//     company: "uaz",
+//     color: "white",
+//     engine: 0.4,
+//     price: 700000,
+//     beaten: true,
+//     year: 1964,
+//     mileage: 3000000,
+//     tech_passport: {
+//       date: 1964,
+//       exp_date: 2023,
+//     },
+//   },
+//   {
+//     model: "velik",
+//     company: "Philips",
+//     color: "red",
+//     engine: "v12",
+//     price: 12000,
+//     beaten: false,
+//     year: 2009,
+//     mileage: 0,
+//     tech_passport: {
+//       date: 2022,
+//       exp_date: 2032,
+//     },
+//   },
+//   {
+//     model: "Porter",
+//     company: "Hyundai",
+//     color: "black",
+//     engine: 2.5,
+//     hp: 145,
+//     price: 31000,
+//     beaten: false,
+//     fuel_comsumption: 10.2,
+//     year: 2023,
+//     mileage: 100,
+//     tech_passport: {
+//       date: 2023,
+//       exp_date: 2033,
+//     },
+//   },
+//   {
+//     model: "Tahoe",
+//     company: "CHEVROLET",
+//     color: "black",
+//     engine: {
+//       capacity: "5.3",
+//       horsePower: "343hp",
+//     },
+//     price: 110000,
+//     beaten: false,
+//     year: 2022,
+//     mileage: 0,
+//     tech_passport: {
+//       date: 2022,
+//       expDate: 2032,
+//     },
+//   },
+//   {
+//     model: "Mercedes-Benz CLS",
+//     company: "Mercedes-Benz",
+//     color: "black",
+//     engine: 3.0,
+//     price: 110000,
+//     beaten: false,
+//     year: 2023,
+//     mileage: 200,
+//     tech_passport: {
+//       date: 2023,
+//       exp_date: 2033,
+//     },
+//   },
+//   {
+//     model: "malibu",
+//     company: "Chevrolet",
+//     color: "sky rim",
+//     engine: 2.4,
+//     price: 15000,
+//     year: 2022,
+//     clash: false,
+//     mileage: 1500,
+//     tech_passport: {
+//       date: 2022,
+//       exp_date: 2054,
+//     },
+//   },
+//   {
+//     model: "matiz",
+//     company: "general motors",
+//     color: "yellow",
+//     engine: 0.8,
+//     price: 4000,
+//     beaten: false,
+//     year: 2018,
+//     mileage: 5000,
+//     tech_passport: {
+//       date: 2018,
+//       exp_date: 2028,
+//     },
+//   },
+//   {
+//     model: "porter",
+//     company: "hyundai",
+//     color: "black",
+//     engine: 2.5,
+//     hp: 145,
+//     price: 31000,
+//     beaten: false,
+//     fuel_comsumption: 10.2,
+//     year: 2023,
+//     mileage: 100,
+//     tech_passport: {
+//       date: 2023,
+//       exp_date: 2033,
+//     },
+//   },
+//   {
+//     model: "bmw_x7",
+//     year: 2023,
+//     price: 85000,
+//     engine: 6.5,
+//     Drivetrain: "All Wheel Drive",
+//     Transmission_Order_Code: "2TB",
+//     engine: "3000cc",
+//     power: "375Bhp",
+//     Seating_Capacity: 6,
+//     Mileage: "11.29 - 14 kmpl",
+//     Fuel: "diesel",
+//     horsepower: 1000,
+//     beaten: false,
+//     color: "black",
+//   },
+// ];
+
+// let currYear = 2024;
+
+// let newcars = [];
+// let companyas = [
+//   {
+//     chevrolet: [],
+//   },
+//   {
+//     generalmotors: [],
+//   },
+//   {
+//     hyundai: [],
+//   },
+// ];
+
+// for (let i = 0; i < cars.length; i++) {
+//   if (cars[i].company === "Сhevrolet") {
+//     companyas[0].chevrolet.push(cars[i]);
+//   } else if (cars[i].company === "general motors") {
+//     companyas[1].generalmotors.push(cars[i]);
+//   } else if (cars[i].company === "hyundai") {
+//     companyas[2].hyundai.push(cars[i]);
+//   }
+// }
+
+// console.log(companyas);
+// // for (let i = 0; i < cars.length; i++) {
+// //  if (cars[i].year > 2022) {
+// //   newcars.push(cars[i])
+// //  }
+// // }
+// // console.log(newcars);
+
+let ask;
+let pop = 4;
+do {
+  ask = +prompt(`Введите пароль у тебя есть ${pop} попытки`);
+  pop--;
+} while (ask != 1111 && pop > 0);
+
+let users = [
   {
-    model: "malibu",
-    mark: "gm",
-    price: 22000,
-    year: 2018,
-    mileage: 64000,
-    horsePower: 212,
+    id: 1,
+    name: "Samir",
+    tax: 12,
+    budget: 400,
+    expenses: [100, 120, 170],
   },
   {
-    model: "cobalt",
-    mark: "gm",
-    price: 11000,
-    year: 2023,
-    mileage: 4000,
-    horsePower: 190,
+    id: 2,
+    name: "Safina",
+    tax: 9,
+    budget: 1700,
+    expenses: [300, 200, 600],
   },
   {
-    model: "cls-63",
-    mark: "mercedes-benz",
-    price: 64000,
-    year: 2010,
-    mileage: 120000,
-    horsePower: 450,
+    id: 3,
+    name: "Leyla",
+    tax: 9,
+    budget: 1500,
+    expenses: [180, 240, 500],
   },
   {
-    model: "competition",
-    mark: "BMW",
-    price: 120000,
-    year: 2020,
-    mileage: 20000,
-    horsePower: 520,
-  },
-  {
-    model: "k5",
-    mark: "kia",
-    price: 32000,
-    year: 2024,
-    mileage: 1000,
-    horsePower: 240,
+    id: 4,
+    name: "Azim",
+    tax: 12,
+    budget: 180,
+    expenses: [40, 30, 10],
   },
 ];
-let faster = cars[0];
-let mil = cars[0];
-let young = cars[0];
-cars.forEach((care) => {
-  if (care.horsePower > faster.horsePower) {
-    faster = care;
-  }
-  if (care.mileage > mil.mileage) {
-    mil = care;
-  }
-  if (care.year > young.year) {
-    young = care;
-  }
-});
 
-console.log(faster, mil, young);
+let successfull = [];
+let unsuccessfull = [];
+let rich = 0;
+let poor = users[0].budget
+for (let user of users) {
+  let totalEx = 0;
+  let TaxSum = (user.tax / 100) * user.budget;
+  totalEx += TaxSum;
+
+  for (let expense of user.expenses) {
+    totalEx += expense;
+  }
+
+  if (totalEx > user.budget) {
+    unsuccessfull.push(user);
+  } else {
+    successfull.push(user);
+  }
+
+  
+  if (user.budget - totalEx > rich) {
+    rich = user;
+  }
+  if (user.budget - totalEx < poor) {
+    poor = user;
+  }
+  console.log(totalEx);
+  
+}
+console.log(rich, poor);
